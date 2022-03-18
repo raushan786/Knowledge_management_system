fa9aef97b26e220c1dfb0f4a127a633423fa9084
Raushan Kumar <54477740+raushan786@users.noreply.github.com>
2022-03-18 20:27:30 +0530

    Update facultylogin.php

--- facultylogin.php
+++ facultylogin.php
@@ -1,12 +1,12 @@
 <?php
-ob_start ();
-session_start();
-require "php/config.php";
-require_once "php/functions.php";
-$user = new login_registration_class();
-if($user->get_faculty_session()){
-	header('Location: class_att_fc.php');
-	exit();
+// ob_start ();
+// session_start();
+// require "php/config.php";
+// require_once "php/functions.php";
+// $user = new login_registration_class();
+// if($user->get_faculty_session()){
+// 	header('Location: class_att_fc.php');
+// 	exit();
 }
 ?>
 
