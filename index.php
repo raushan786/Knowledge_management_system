a68a3e95d2d954fcab7a33392865cbcdd80d335f
Raushan Kumar <54477740+raushan786@users.noreply.github.com>
2022-03-18 20:06:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 ob_start ();
 session_start();
-require "php/config.php";
-require_once "php/functions.php";
+// require "php/config.php";
+// require_once "php/functions.php";
 $user = new login_registration_class();
 if($user->get_admin_session()){
 	header('Location: admin.php');
