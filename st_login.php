92e6ba2574ae9712763fa86047ef6baa2f49b861
Raushan Kumar <54477740+raushan786@users.noreply.github.com>
2022-03-18 20:39:04 +0530

    Update st_login.php

--- st_login.php
+++ st_login.php
@@ -1,13 +1,13 @@
 <?php
-ob_start ();
-session_start();
-require "php/config.php";
-require_once "php/functions.php";
-$user = new login_registration_class();
-if($user->getsession()){
-	header('Location: st_profile.php');
-	exit();
-}
+// ob_start ();
+// session_start();
+// require "php/config.php";
+// require_once "php/functions.php";
+// $user = new login_registration_class();
+// if($user->getsession()){
+// 	header('Location: st_profile.php');
+// 	exit();
+// }
 ?>
 <?php 
 $pageTitle = "Student login";
