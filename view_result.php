dba51cc8fe0fb097d0437a23c6a47d1344fd303b
Raushan Kumar <54477740+raushan786@users.noreply.github.com>
2022-03-18 20:42:37 +0530

    Update view_result.php

--- view_result.php
+++ view_result.php
@@ -1,19 +1,19 @@
 <?php
-session_start();
-	require "php/config.php";
-	require_once "php/functions.php";
-	$user = new login_registration_class();
-	$admin_id = $_SESSION['admin_id'];
-	$admin_name = $_SESSION['admin_name'];
+// session_start();
+// 	require "php/config.php";
+// 	require_once "php/functions.php";
+// 	$user = new login_registration_class();
+// 	$admin_id = $_SESSION['admin_id'];
+// 	$admin_name = $_SESSION['admin_name'];
 
-	if(!$user->get_admin_session()){
-		header('Location: index.php');
-		exit();
-	}
-	if(isset($_REQUEST['vr'])){
-		$stid = $_REQUEST['vr'];
-		$name = $_REQUEST['vn'];
-	}
+// 	if(!$user->get_admin_session()){
+// 		header('Location: index.php');
+// 		exit();
+// 	}
+// 	if(isset($_REQUEST['vr'])){
+// 		$stid = $_REQUEST['vr'];
+// 		$name = $_REQUEST['vn'];
+// 	}
 ?>	
 <?php 
 $pageTitle = "Student Result";
